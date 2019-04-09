88f73b696a228d4bfe2db2b1af6c49c46bdfbfb2
byhunterx <49447466+byhunterx@users.noreply.github.com>
2019-04-09 16:04:59 +0100

    Delete linkit.py

--- linkit.py
+++
@@ -1,5 +0,0 @@
-import webbrowser
-if len(intentMessage.slots.pdts) > 0:
-   val = intentMessage.slots.pdts.first().rawValue
-   url = "http://360.topnegoce.com:8000/new/admin/R_Banc_ass/php/SNIPS_ASSET/response.php?intent=pr&val="+val
-   webbrowser.open_new_tab(url)
